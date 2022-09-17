fab8134d5f8e576e3082faa5f2ced40b0ad65550
TyroAurum <88623636+TyroAurum@users.noreply.github.com>
2022-09-17 09:53:44 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ root.render(
   <BrowserRouter>
   <Routes>
     <Route exact path='/' element={<App />} />
-    <Route exact path='/loggedin' element={ <ResultCard/>} />
+    <Route exact path='/results/IT' element={ <ResultCard/>} />
   </Routes>
   </BrowserRouter>
 );
